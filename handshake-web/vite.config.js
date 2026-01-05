748ac3b1161bf653551b82176e8e7412743c370e
张玮维 <zhangweiwei.erica@bytedance.com>
2026-01-05 10:21:19 +0800

    Fix base path for custom domain

--- handshake-web/vite.config.js
+++ handshake-web/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/SecureTransfer/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
